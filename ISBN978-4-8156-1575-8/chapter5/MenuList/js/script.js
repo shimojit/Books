2145e41fbc3796ae9a16291ea633168a2994676d
shimojit <0liberty4life@gmail.com>
2024-08-06 15:38:02 +0900

    chapter5 all completes.

--- ISBN978-4-8156-1575-8/chapter5/MenuList/js/script.js
+++ ISBN978-4-8156-1575-8/chapter5/MenuList/js/script.js
@@ -1,15 +1,40 @@
 const menu = document.querySelector('#menu');
 
 const lists = [
-  'strawberry.jpg',
-  'lime.jpg',
-  'mango.jpg',
-  'lemon.jpg',
-  'fig.jpg',
-  'apple.jpg',
+  {
+    name: 'イチゴ',
+    img: 'strawberry.jpg',
+    price: 450,
+  },
+  {
+    name: 'ライム',
+    img: 'lime.jpg',
+    price: 400,
+  },
+  {
+    name: 'マンゴー',
+    img: 'mango.jpg',
+    price: 500,
+  },
+  {
+    name: 'レモン',
+    img: 'lemon.jpg',
+    price: 400,
+  },
+  {
+    name: 'イチジク',
+    img: 'fig.jpg',
+    price: 500,
+  },
+  {
+    name: 'リンゴ',
+    img: 'apple.jpg',
+    price: 400,
+  },
 ];
 
 for (let i = 0; i < lists.length; i++) {
-  const content = `<div><img src="images/${lists[i]}" alt=""></div>`;
+  const {name, img, price} = lists[i];
+  const content = `<div><img src="images/${img}" alt=""><h2>${name}</h2><p>${price}円</p></div>`;
   menu.insertAdjacentHTML('beforeend', content);
 }
