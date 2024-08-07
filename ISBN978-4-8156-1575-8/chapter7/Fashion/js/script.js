eaacf4456e30354e2d33aed5f336141805476c0d
shimojit <0liberty4life@gmail.com>
2024-08-07 16:39:06 +0900

    chapter7-3 trying.

--- ISBN978-4-8156-1575-8/chapter7/Fashion/js/script.js
+++ ISBN978-4-8156-1575-8/chapter7/Fashion/js/script.js
@@ -1,3 +1,20 @@
 /*
 ローディングから画面遷移
 ================================================== */
+const loadingAreaGrey = document.querySelector('#loading');
+//console.log(loadingAreaGrey);
+window.addEventListener('load', () => {
+  // ローディング中（グレースクリーン）
+  loadingAreaGrey.animate(
+    {
+      opacity: [1, 0],
+      visibility: 'hidden',
+    },
+    {
+      duration: 2000,
+      delay: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+});
