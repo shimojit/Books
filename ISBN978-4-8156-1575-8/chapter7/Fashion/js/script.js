261df93349c3a5065047926ab80759279ad6fe89
shimojit <0liberty4life@gmail.com>
2024-08-07 17:38:12 +0900

    chapter7-5 completes.

--- ISBN978-4-8156-1575-8/chapter7/Fashion/js/script.js
+++ ISBN978-4-8156-1575-8/chapter7/Fashion/js/script.js
@@ -2,7 +2,9 @@
 ローディングから画面遷移
 ================================================== */
 const loadingAreaGrey = document.querySelector('#loading');
-//console.log(loadingAreaGrey);
+const loadingAreaGreen = document.querySelector('#loading-screen');
+const loadingText = document.querySelector('#loading p');
+
 window.addEventListener('load', () => {
   // ローディング中（グレースクリーン）
   loadingAreaGrey.animate(
@@ -13,6 +15,38 @@ window.addEventListener('load', () => {
     {
       duration: 2000,
       delay: 1200,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+
+  // ローディング中（薄緑スクリーン）
+  loadingAreaGreen.animate(
+    {
+      translate: ['0 100vh', '0 0', '0 -100vh'],
+    },
+    {
+      duration: 2000,
+      delay: 800,
+      easing: 'ease',
+      fill: 'forwards',
+    }
+  );
+
+  // ローディング中のテキスト
+  loadingText.animate(
+    [
+      {
+        opacity: 1,
+        offset: 0.8, // 80%
+      },
+      {
+        opacity: 0,
+        offset: 1, // 100%
+      }
+    ],
+    {
+      duration: 1200,
       easing: 'ease',
       fill: 'forwards',
     }
