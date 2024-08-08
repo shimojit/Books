c9f6ae8ce3fcb1ff72d46aa29d84cdc2ae02a77d
shimojit <0liberty4life@gmail.com>
2024-08-08 18:58:43 +0900

    chapter7 all completes.

--- ISBN978-4-8156-1575-8/chapter7/Fashion/js/script.js
+++ ISBN978-4-8156-1575-8/chapter7/Fashion/js/script.js
@@ -112,7 +112,27 @@ thumbImages.forEach((thumbImage) => {
 スクロールで要素を表示
 ================================================== */
 // 監視対象が範囲内に現れたら実行する動作
-const animateFade = () => {};
+const animateFade = (entries, obs) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.animate(
+        {
+          opacity: [0, 1],
+          filter: ['blur(0.4rem)', 'blur(0)'],
+          translate: ['0.4rem', 0],
+        },
+        {
+          duration: 2000,
+          easing: 'ease',
+          fill: 'forwards',
+        }
+      );
+
+      // 一度ふわっと表示されたら監視をやめる
+      obs.unobserve(entry.target);
+    }
+  });
+};
 
 // 監視設定
 const fadeObserver = new IntersectionObserver(animateFade);
