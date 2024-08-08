b1b28965d2ef74badd5f6afbe5552e01317e4444
shimojit <0liberty4life@gmail.com>
2024-08-08 13:07:55 +0900

    chapter7-8 completes.

--- ISBN978-4-8156-1575-8/chapter7/Fashion/js/script.js
+++ ISBN978-4-8156-1575-8/chapter7/Fashion/js/script.js
@@ -52,3 +52,16 @@ window.addEventListener('load', () => {
     }
   );
 });
+
+/*
+画像ギャラリー
+================================================== */
+const mainImage = document.querySelector('.gallery-image img');
+const thumbImages = document.querySelectorAll('.gallery-thumbnails img');
+
+thumbImages.forEach((thumbImage) => {
+  thumbImage.addEventListener('mouseover', (event) => {
+    mainImage.src = event.target.src;
+    mainImage.animate({ opacity: [0, 1] }, 500);
+  });
+});
